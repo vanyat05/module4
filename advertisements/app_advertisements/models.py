4ebb5776bdf2243c6a4c1efdcd7e14c9af183908
ivanchik <vnthnnk@gmail.com>
2023-08-09 14:25:58 +0700

    hw8

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -8,7 +8,7 @@ class Advertisement(models.Model):
     price = models.DecimalField("цена", max_digits=10, decimal_places=2)
     auction = models.BooleanField("торг", help_text="Отметьте, если тогр уместен")
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     @admin.display(description='дата создания')
     def created_date(self):
@@ -19,6 +19,16 @@ class Advertisement(models.Model):
                 '<span style="color: green; font-weight:bold;">Сегодня в {}</span>', created_time
             )
         return self.created_at.strftime("%d.%n.%Y в %H:%M:%S")
+
+    @admin.display(description='дата последнего обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            created_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight:bold;">Сегодня в {}</span>', created_time
+            )
+        return self.updated_at.strftime("%d.%n.%Y в %H:%M:%S")
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
